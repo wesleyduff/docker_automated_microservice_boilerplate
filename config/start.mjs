c17a37c9f34d489e7b96b843509600b0600426d1
Wes Duff <wesley.a.duff@gmail.com>
2019-05-23 11:51:26 -0600

    Cleanup of config for start

--- config/start.mjs
+++ config/start.mjs
@@ -1,24 +1,5 @@
 export default {
     name: 'DEMO',
-    environment: {
-        minikube: false,
-        local: true
-    },
-    stash: {
-        api_token: 'OTYzMjcyNTcxMTI5OsV+WK9DnO7JKvqmcM/V6uvNd0CB'
-    },
-    docker: {
-        local_docker_file_path: './Docker/docker_files/Dockerfile_local',
-        image: {
-            name: 'localexample2Start', //must be LOWERCASE
-            version: 'v1'
-        },
-        volumes: {
-            path_source_on_container: '/data',
-            path_source_on_host: '/' //$PWD is used as the root. Add any further drill down into rave-web as you like
-        },
-        save_to_machine: true //Should we save the docker image to our machine or to minikube
-    },
     mongo: {
         discovery: "static",
         data: "RavenData",
